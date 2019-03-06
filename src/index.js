68482d0c553754d5cae6411d751009c11a66ddc1
Daniel Ortiz <do973658@wtstudents.wtamu.edu>
2019-03-06 14:16:09 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'popper.js/dist/popper.js'
+import 'jquery/dist/jquery.js'
 import './index.css';
+import 'react-mapbox-gl';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
